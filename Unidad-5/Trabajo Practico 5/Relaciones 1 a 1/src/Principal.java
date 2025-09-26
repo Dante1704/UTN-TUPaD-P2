a12d227db652d9f3ad9c65d7e567b25318f17d64
Dante Kaddarian <dante.nkaddarian@gmail.com>
2025-09-26 12:50:48 -0300

    Mejorada la organizacion del repositorio

--- Unidad-5/Trabajo Practico 5/Relaciones 1 a 1/src/Principal.java
+++ Unidad-5/Trabajo Practico 5/Relaciones 1 a 1/src/Principal.java
@@ -11,7 +11,8 @@ public class Principal {
      */
     public static void main(String[] args) {
         //Ejercicio 1
-//        Pasaporte pasaporte = new Pasaporte("AGR5617A", "2025/09/17", "Foto 1", "PNG");
+//        Pasaporte pasaporte = new Pasaporte("AGR5617A", "2025/09/17", 
+//        "Foto 1", "PNG");
 //        System.out.println(pasaporte);
 //        pasaporte.mostrarFoto();
 //        
@@ -25,12 +26,15 @@ public class Principal {
         
         //Ejercicio 2
 //        Bateria bateria = new Bateria("EB-BG950ABE", "4000 mAh");
-//        Celular celular = new Celular("358762091234567", "Nokia", "1100", bateria); //agregacion
+//        Celular celular = new Celular("358762091234567", "Nokia", "1100",
+//        bateria); //agregacion
 //        System.out.println(celular);
 //        celular.mostrarBateria();
-//        celular.setBateria(null); //elimino la referencia que es equivalente a remover la bateria
+//        celular.setBateria(null); //elimino la referencia que es equivalente 
+//        a remover la bateria
 //        celular.mostrarBateria();
-//        System.out.println(bateria); //se quito la bateria del celular pero igual la bateria sigue existiendo
+//        System.out.println(bateria); //se quito la bateria del celular pero 
+//        igual la bateria sigue existiendo
         
 //        Usuario usuario = new Usuario("Dante Kaddarian", "35459831");
 //        usuario.setCelular(celular);
@@ -41,8 +45,10 @@ public class Principal {
 //        System.out.println(celular.getUsuario().getNombre());
         
         //Ejercicio 3
-//        Editorial editorial = new Editorial("Silverleaf Publishing", "4512 Maple Avenue, Suite 200, Portland, OR 97205, USA");
-//        Libro libro = new Libro("Como ganar amigos e influir en las personas", "978-1-23456-789-7", editorial);
+//        Editorial editorial = new Editorial("Silverleaf Publishing", 
+//        "4512 Maple Avenue, Suite 200, Portland, OR 97205, USA");
+//        Libro libro = new Libro("Como ganar amigos e influir en las personas",
+//        "978-1-23456-789-7", editorial);
 //        System.out.println(libro);
 //        libro.mostrarEditorial();
 //        libro.setEditorial(null);
@@ -55,28 +61,35 @@ public class Principal {
         
         //Ejercicio 4
 //        Banco banco = new Banco("Santander", "24-35459831-7");
-//        TarjetaDeCredito tarjeta = new TarjetaDeCredito("4567456788590016", "02/30", banco);
+//        TarjetaDeCredito tarjeta = new TarjetaDeCredito("4567456788590016", 
+//        "02/30", banco);
 //        tarjeta.mostrarBanco();
 //        Cliente cliente = new Cliente("Dante Kaddarian", "35459831");
 //        tarjeta.setCliente(cliente);
 //        System.out.println("Tarjeta de Credito: " + tarjeta.getNumero());
 //        System.out.println("Cliente: " + tarjeta.getCliente().getNombre());
-//        System.out.println("Tarjeta Asociada al cliente: " + cliente.getTarjetaDeCredito().getNumero());
+//        System.out.println("Tarjeta Asociada al cliente: " + 
+//        cliente.getTarjetaDeCredito().getNumero());
         
         //Ejercicio 5
-//          Computadora computadora = new Computadora("HP", "156-G", "ASUS", "h610m-k");
+//          Computadora computadora = new Computadora("HP", "156-G", "ASUS", 
+//          "h610m-k");
 //          System.out.println(computadora);
 //          
-//          Propietario propietario = new Propietario("Dante Kaddarian", "35459831");
+//          Propietario propietario = new Propietario("Dante Kaddarian", 
+//          "35459831");
 //          computadora.setPropietario(propietario);
 //          
 //          System.out.println("Computadora: " + computadora.getMarca());
-//          System.out.println("Propietario: " + computadora.getPropietario().getNombre());
-//          System.out.println("Computadora Asociada al Propietario: " + propietario.getComputadora().getMarca());
+//          System.out.println("Propietario: " + 
+//          computadora.getPropietario().getNombre());
+//          System.out.println("Computadora Asociada al Propietario: " + 
+//          propietario.getComputadora().getMarca());
 
         //Ejercicio 6
 //            Mesa mesa = new Mesa(16, 6);
-//            ClienteRestaurant cliente = new ClienteRestaurant("Dante Kaddarian", "1134961991");
+//            ClienteRestaurant cliente = new ClienteRestaurant(
+//            "Dante Kaddarian", "1134961991");
 //            Reserva reserva = new Reserva("23/09/25", "20hs" , mesa);
 //            reserva.setCliente(cliente);
 //            
@@ -92,7 +105,8 @@ public class Principal {
 
         //Ejercicio 7
 //              Motor motor = new Motor("Cummins", "MTR-8F2X-394721");
-//              Vehiculo vehiculo = new Vehiculo("AK116II", "reightliner Cascadia 126", motor);
+//              Vehiculo vehiculo = new Vehiculo("AK116II", 
+//              "reightliner Cascadia 126", motor);
 //              
 //              vehiculo.mostrarMotor();
 //              vehiculo.setMotor(null);
@@ -102,23 +116,30 @@ public class Principal {
 //              Conductor conductor = new Conductor("Dante Kaddarian", "B1");
 //              conductor.setVehiculo(vehiculo);
 //              System.out.println("Conductor: " + conductor.getNombre());
-//              System.out.println("Vehiculo modelo: " + conductor.getVehiculo().getModelo());
-//              System.out.println("Conductor Asociado al Vehiculo: " + vehiculo.getConductor().getNombre());
+//              System.out.println("Vehiculo modelo: " + 
+//              conductor.getVehiculo().getModelo());
+//              System.out.println("Conductor Asociado al Vehiculo: " + 
+//              vehiculo.getConductor().getNombre());
         //Ejercicio 8
-//                Persona propietario = new Persona("Dante Kaddarian", "dante.kaddarian@tupad.utn.edu.ar");
-//                Documento documento = new Documento("Titulo de Propiedad", "Contenido", "a830d7beb04eb7549ce990fb7dc962e499a27230d", "24/09/25", propietario);
+//              Persona propietario = new Persona("Dante Kaddarian", 
+//              "dante.kaddarian@tupad.utn.edu.ar");
+//              Documento documento = new Documento("Titulo de Propiedad", 
+//              "Contenido", "a830d7beb04eb7549ce990fb7dc962e499a27230d", 
+//              "24/09/25", propietario);
 //                
-//                documento.mostrarFirma();
+//              documento.mostrarFirma();
 //                
-//                documento.mostrarPersona();
-//                documento.setPersona(null);
-//                documento.mostrarPersona();
-//                System.out.println(propietario);
+//              documento.mostrarPersona();
+//              documento.setPersona(null);
+//              documento.mostrarPersona();
+//              System.out.println(propietario);
 
         //Ejercicio 9
 //                CitaMedica cita = new CitaMedica("24/09/25",21);
-//                Paciente paciente = new Paciente("Dante Kaddarian", "SancorSalud");
-//                Profesional profesional = new Profesional("Franco Arpone", "Odontologia");
+//                Paciente paciente = new Paciente("Dante Kaddarian", 
+//                "SancorSalud");
+//                Profesional profesional = new Profesional("Franco Arpone", 
+//                "Odontologia");
 //                cita.setPaciente(paciente);
 //                cita.setProfesional(profesional);
 //                
@@ -130,8 +151,10 @@ public class Principal {
 //                System.out.println(paciente);
 
         //Ejercicio 10
-//                CuentaBancaria cuenta = new CuentaBancaria("000006546879967", 10000.0, "561g6rgrgg?", "24/09/25");
-//                TitularCuenta titular = new TitularCuenta("Dante Kaddarian", "35459831");
+//                CuentaBancaria cuenta = new CuentaBancaria("000006546879967", 
+//                10000.0, "561g6rgrgg?", "24/09/25");
+//                TitularCuenta titular = new TitularCuenta("Dante Kaddarian", 
+//                "35459831");
 //                
 //                cuenta.mostrarClave();
 //                cuenta.setClave("5gf(((0??/(", "24/09/25");
@@ -139,8 +162,10 @@ public class Principal {
 //                
 //                cuenta.setTitular(titular);
 //                System.out.println("Cuenta Bancaria: " + cuenta.getCbu());
-//                System.out.println("Titular Asociado: " + cuenta.getTitular().getNombre());
-//                System.out.println("Cuenta Asociada al Titular:" + titular.getCuenta().getCbu());
+//                System.out.println("Titular Asociado: " + 
+//                cuenta.getTitular().getNombre());
+//                System.out.println("Cuenta Asociada al Titular:" + 
+//                titular.getCuenta().getCbu());
            
     }
 
